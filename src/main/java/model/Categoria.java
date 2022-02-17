d7e1eab5786c8de248cab540701802c4b428981f
Rodrigo Prado <digosilva.prado@gmail.com>
2022-02-17 19:20:36 -0300

    Primeiro Deploy

---
+++ src/main/java/model/Categoria.java
@@ -0,0 +1,31 @@
+package model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "categoria")
+public class Categoria {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int codigo;
+	@Column(length = 100)
+	private String nome;
+	
+	public int getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}		
+}
